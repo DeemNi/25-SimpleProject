a6a875bf3038c89c40fb2603255581e49d9d2922
Nikita Demianov <hourlystory@gmail.com>
2024-03-02 19:50:46 +0200

    add a star rating component

---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,44 @@
+
+import { useState } from 'react';
+import { FaStar } from "react-icons/fa";
+import './style.css'
+
+export default function StarRating({numberOfStars = 5}) {
+    
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex){
+        console.log(getCurrentIndex);
+
+
+    }
+
+    function handleMouseEnter(getCurrentIndex){
+        setHover(getCurrentIndex);
+    }
+
+    function handleMouseLeave(){
+        setHover(rating);
+    }
+
+    return(
+        <div className="star-rating">
+
+            {
+                [...Array(numberOfStars)].map((_, index) => {
+
+                    index += 1;
+                    return <FaStar 
+                        key={index}
+                        className={index <= (hover || rating) ? 'active' : 'inactive'}
+                        onClick={() => handleClick(index)}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeft={() => handleMouseLeave()}
+                        size={40}
+                    />
+                })
+            }
+        </div>
+    )
+}
